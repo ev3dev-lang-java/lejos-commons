35a4e0025a1718b31e4929b186a209df8e84f7f6
Juan Antonio Breña Moral <bren@juanantonio.info>
2017-05-27 20:45:08 +0100

    Removing some stop methods provided by EV3Dev

--- src/main/java/lejos/robotics/BaseMotor.java
+++ src/main/java/lejos/robotics/BaseMotor.java
@@ -26,22 +26,7 @@ public interface BaseMotor {
      * This is not the same as stopping, which locks the rotor.
      */
     void flt();
-
-    /**
-     * Removes power from the motor and creates a passive electrical load.
-     * This is usually done by shorting the motor terminals together.
-     * This load will absorb the energy from the rotation of the motors and
-     * cause the motor to stop more quickly than coasting.
-     */
-    void brake();
-
-    /**
-     * Causes the motor to actively try to hold the current position.
-     * If an external force tries to turn the motor, the motor will “push back”
-     * to maintain its position.
-     */
-    void hold();
-
+    
     /**
     * Return <code>true</code> if the motors is moving.
     *
